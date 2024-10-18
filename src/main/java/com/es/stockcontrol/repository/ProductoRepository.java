629058d3143a94a93688204a7704514c773c4e2b
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2024-10-18 19:27:07 +0200

    Repository Producto getStocks + baja + modify

--- src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
+++ src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
@@ -4,6 +4,10 @@ import com.es.stockcontrol.model.Producto;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
+import java.util.List;
+
 
 public class ProductoRepository {
 
@@ -15,7 +19,43 @@ public class ProductoRepository {
 
         em.getTransaction().begin();
 
-        return em.find(Producto.class, id);
+        Producto p = em.find(Producto.class, id);
+
+        em.close();
+
+        return p;
+
+    }
+
+    public List<Producto> getProductoConStock() {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Query query = em.createQuery("SELECT * FROM Producto WHERE stock != 0 || stock != null");
+
+        List<Producto> listaP = query.getResultList();
+
+        em.close();
+
+        return listaP;
+
+    }
+
+    public List<Producto> getProductoSinStock() {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Query query = em.createQuery("SELECT * FROM Producto WHERE stock == 0 || stock == null");
+
+        List<Producto> listaP = query.getResultList();
+
+        em.close();
+
+        return listaP;
 
     }
 
@@ -31,7 +71,61 @@ public class ProductoRepository {
 
         em.close();
 
-        return getProducto(p.getId());
+        return em.find(Producto.class, p.getId());
+
+    }
+
+    public Producto bajaProducto(String idProducto) {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Producto p = em.find(Producto.class, idProducto);
+
+        em.remove(p);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+        return p;
+
+    }
+
+    public Producto modificarNombreProducto(String idProducto, String nuevoNombre) {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Producto p = em.find(Producto.class, idProducto);
+
+        p.setNombre(nuevoNombre);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+        return em.find(Producto.class, p.getId());
+
+    }
+
+    public Producto modificarStockProducto(String idProducto, Integer nuevoStock) {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Producto p = em.find(Producto.class, idProducto);
+
+        p.setStock(nuevoStock);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+        return em.find(Producto.class, p.getId());
 
     }
 
